161c5d39e3ff1e266462edf1c8cffcb2484a88af
David Anchieta <davidca102@gmail.com>
2016-10-03 11:28:03 -0300

    Adicionei zeros ao fim do sinal 'Analógico' para que sei comprimento fosse igual a 2^m. Assim não deve mais haver erros na execução de PhaseMod

--- NossoCodigo2/convertesinal.m
+++ NossoCodigo2/convertesinal.m
@@ -2,9 +2,12 @@ load('../NossoCodigo/original66khz.mat')
 
 addpath('../SegundoTrabalhoArquivos')
 
+m = ceil(log2(length(original)));
+audio = [original; zeros(2^m - length(original) ,1)] ;
+
+k=6;
 fs8k = 2^13;
 
-x8khz6b = Analog2Digital(original, fs8k, k, 0, 0, fs);
+x8khz6b = Analog2Digital(audio, fs8k, k, 0, 0, fs);
 
 % save '-v6' 'audioDigital8khz6bit.mat' fs8k x8khz6b k
-
